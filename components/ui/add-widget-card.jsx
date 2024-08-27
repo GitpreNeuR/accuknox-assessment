6a2bdec5ca3cecd11e9720d177870f7275f47e0d
GitpreNeuR <95102789+GitpreNeuR@users.noreply.github.com>
2024-08-27 15:41:58 +0530

    Updated add-widget-card.jsx

--- components/ui/add-widget-card.jsx
+++ components/ui/add-widget-card.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import { Card, CardHeader,  CardContent } from "@/components/ui/card";
+import React from 'react';
+import { Card, CardContent} from "@/components/ui/card";
 import { Plus } from 'lucide-react';
 import { useModal } from '../hooks/useModal';
 import { Button } from './button';
 
-
 function AddWidgetCard() {
-  const modal=useModal();
+  const modal = useModal();
+
   return (
-    <>
-    <Card className="relative shadow-xl flex  items-center justify-center w-full h-full">
-      <CardContent className="h-52 w-full ">
-      <div className="w-full h-full items-center justify-center flex">
-      <Button onClick={modal.onOpen} variant="default" className="flex gap-x-2 items-center">
-      <span>Add Widget</span>
-      <Plus className="h-4 w-4 text-gray-900" />
-     </Button>
-     </div>
+    <Card className="relative shadow-xl overflow-hidden  w-full min-h-[19rem] h-full ">
+    
+      <CardContent className="flex items-center justify-center h-full rounded-md ">
+        <Button onClick={modal.onOpen} variant="default" className="flex gap-x-2 items-center">
+          <span>Add Widget</span>
+          <Plus className="h-4 w-4 text-gray-900" />
+        </Button>
       </CardContent>
     </Card>
-    </>
-  )
+  );
 }
 
-export default AddWidgetCard
+export default AddWidgetCard;
